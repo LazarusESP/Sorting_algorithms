79a84ede81275bfe4346ba717d9283e3a054a4fe
LazarusESP <121804382+LazarusESP@users.noreply.github.com>
2023-01-23 17:20:05 +0100

    Update Processing algorithms.py

--- Processing algorithms.py
+++ Processing algorithms.py
@@ -4,7 +4,6 @@ import csv
 def generate_processes(num_processes, mean_execution_time, std_dev_execution_time, mean_arrival_time, std_dev_arrival_time, zero_arrival = False):
   # lista procesów
   processes = []
-  # początkowy czas przyjścia
 
   # iterujemy po wszystkich procesach
   for i in range(num_processes):
@@ -69,38 +68,36 @@ def lcfs_scheduling(processes):
     average_turnaround_time = sum(turnaround_time) / len(turnaround_time)
 
     return (average_waiting_time, average_turnaround_time, counter)
+    
 #generator(ilosc, srednia execution, odchylenie standardowe execution,srednia arrival, odchylenie standardowe arrival,
 #  czy zerować = False)
+
+#GENERATOR
+#
+#
 generator_amount = 10
 execution_mean = 10
 execution_std_dev = 5
 arrival_mean = 20
 arrival_std_dev = 2
 is_zero = False
+#
+#
+
 processes = generate_processes(generator_amount, execution_mean, execution_std_dev,arrival_mean, arrival_std_dev, is_zero)
 processes = sorted(processes, key=lambda x: x[0])
+
 for process in processes:
     print(process, end=' ')
 print()
-#[(2,6),(5,2),(1,8),(0,3),(4,4)]
+
 avg_time, avg_turnaround, time = fcfs_scheduling(processes)
 avg_time2, avg_turnaround2, time2 = lcfs_scheduling(processes)
 print('średni czas oczekiwania fcfs: ', avg_time,'czas programu fcfs: ',time)
 print('średni czas oczekiwania lcfs: ',avg_time2,'czas programu lcfs: ',time2)
-#output = ' '.join(map(str,processes))
-arrival_output = []
-execution_output = []
-for i, (arrival_time, execution_time) in enumerate(processes):
-    arrival_output.append(arrival_time)
-    execution_output.append(execution_time)
-arrival_output_str = ' '.join(map(str,arrival_output))
-execution_output_str = ' '.join(map(str,execution_output))
 
 with open(r'C:\Users\Piotr\Documents\data_processing.csv', "a", newline = '') as csvfile:
     thewriter = csv.writer(csvfile)
     data = [generator_amount, avg_time, avg_time2,avg_turnaround , avg_turnaround2]
     thewriter.writerow(data)
-
-
-#print('\nsrednia arrival: ',sum(arrival_output)/len(arrival_output))
-#print('\nsrednia execution: ',sum(execution_output)/len(execution_output))
+#omówienie algorytmu
